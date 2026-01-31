75903168dd05f508423ad9d971362a0d458de978
伊利亚·德扬卡·葉泽洛夫斯基 <ilyayezelovsky@gmail.com>
2026-01-31 17:39:09 +0800

    Completed random

--- src/org/lokova/classroom/Random.java
+++ src/org/lokova/classroom/Random.java
@@ -1,5 +1,6 @@
 package org.lokova.classroom;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Random {
@@ -18,11 +19,7 @@ public class Random {
 		inner = new java.util.Random(seed);
 	}
 
-	private Student generateFrom(List<Student> list) {
-		return list.get(inner.nextInt(list.size()));
-	}
-
-	public Student next(Sex sex) {
+	private Student generate(Sex sex) {
 		if (sex == Sex.MALE) {
 			return generateFrom(Student.getBoys());
 		}
@@ -32,12 +29,71 @@ public class Random {
 		return generateFrom(Student.getAll());
 	}
 
+	private List<Student> generate(Sex sex, int count, boolean duplicate) {
+		if (sex == Sex.MALE) {
+			return generateFrom(Student.getBoys(), count, duplicate);
+		}
+		if (sex == Sex.FEMALE) {
+			return generateFrom(Student.getGirls(), count, duplicate);
+		}
+		return generateFrom(Student.getAll(), count, duplicate);
+	}
+
+	private Student generateFrom(List<Student> list) {
+		return list.get(inner.nextInt(list.size()));
+	}
+
+	private List<Student> generateFrom(List<Student> list, int count, boolean duplicate) {
+		List<Student> result = new ArrayList<>();
+		if (duplicate) {
+			for (int i = 0; i < count; i++) {
+				result.add(generateFrom(list));
+			}
+		} else {
+			Student candidate = null;
+			while (result.size() < count) {
+				if (!result.contains(candidate = generateFrom(list))) {
+					result.add(candidate);
+				}
+			}
+		}
+		return result;
+	}
+
+	public Student next() {
+		return generate(null);
+	}
+
+	public List<Student> next(int count) {
+		return next(count, false);
+	}
+
+	public List<Student> next(int count, boolean duplicate) {
+		return generate(null, count, duplicate);
+	}
+
 	public Student nextBoy() {
-		return next(Sex.MALE);
+		return generate(Sex.MALE);
+	}
+
+	public List<Student> nextBoy(int count) {
+		return nextBoy(count, false);
+	}
+
+	public List<Student> nextBoy(int count, boolean duplicate) {
+		return generate(Sex.MALE, count, duplicate);
 	}
 
 	public Student nextGirl() {
-		return next(Sex.FEMALE);
+		return generate(Sex.FEMALE);
+	}
+
+	public List<Student> nextGirl(int count) {
+		return nextGirl(count, false);
+	}
+
+	public List<Student> nextGirl(int count, boolean duplicate) {
+		return generate(Sex.FEMALE, count, duplicate);
 	}
 
 }
